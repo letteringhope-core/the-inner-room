7a43f8bd689fa0a3ba244fed1976ee8557670c55
Georgy <georgy.roy@hotmail.com>
2026-07-15 10:24:48 +0530

    Support GitHub Pages deployment alongside Firebase Hosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: process.env.GH_PAGES ? '/the-inner-room/' : '/',
   plugins: [
     react(),
     VitePWA({
